0dc40da44677f703abcd3afdb6533d4b186cb7be
prejuk <93695329+prejuk@users.noreply.github.com>
2024-01-04 18:55:58 -0800

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Preju Kanuparthy on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
